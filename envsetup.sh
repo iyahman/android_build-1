e8cf5dd5883a4d50e704f8ab85ac299f2b68badd
Iyahman <ultimenattydreadz@gmail.com>
2017-07-31 01:30:11 +0200

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -217,7 +217,7 @@ function setpaths()
             ;;
         x86_64) toolchaindir=x86/x86_64-linux-android-$targetgccversion/bin
             ;;
-        arm) toolchaindir=arm/arm-linux-androideabi-UBER-5.x/bin
+        arm) toolchaindir=arm/arm-linux-androideabi-$targetgccversion/bin
             ;;
         arm64) toolchaindir=aarch64/aarch64-linux-android-UBER-5.x/bin;
                toolchaindir2=arm/arm-linux-androideabi-UBER-5.x/bin
